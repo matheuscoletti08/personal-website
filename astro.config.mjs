ce28d9520f937d68f784f707caa4a47283b1fa02
Coletti <matheus.coletti08@gmail.com>
2026-07-31 21:42:38 -0300

    feat: update site URL and remove base path from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://matheuscoletti08.github.io',
-	base: '/personal-website',
+	site: 'https://matheuscoletti.is-a.dev',
 
 	i18n: {
 		defaultLocale: 'pt',
 		locales: ['en', 'pt'],
 		routing: {
-		prefixDefaultLocale: true,
-		redirectToDefaultLocale: false,
+			prefixDefaultLocale: true,
+			redirectToDefaultLocale: false,
 		},
 	},
 });
